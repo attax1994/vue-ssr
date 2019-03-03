c22d24644ce711ba23ff81ccef6b70e4efdd5821
attax1994 <lqytyui@qq.com>
2019-03-03 15:46:27 +0800

    refactor(static.middleware): optimize fileName assignment format

--- middleware/static.middleware.js
+++ middleware/static.middleware.js
@@ -4,8 +4,7 @@ const path = require('path')
 module.exports = async (ctx, next) => {
   const {url} = ctx
   if ((ctx.method === 'HEAD' || ctx.method === 'GET') && /\.(js|css)$/.test(url)) {
-    const fileNameStack = url.split('/')
-      , fileName = fileNameStack[fileNameStack.length - 1]
+    const fileName = url.split('/').splice(-1).toString()
     ctx.body = fs.readFileSync(path.join(__dirname, '../assets/client', fileName), 'UTF-8')
     return
   }
